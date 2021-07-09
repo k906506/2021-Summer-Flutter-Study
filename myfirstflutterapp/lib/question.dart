d017a2d10a1e4e72e5d2aeae233e81585e6d6680
k906506 <k906506@naver.com>
2021-07-09 15:35:23 +0900

    Question 클래스 생성 후 Main과 연결

---
+++ myfirstflutterapp/lib/question.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  final String _questionText;
+
+  Question(this._questionText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _questionText,
+      style: TextStyle(fontSize: 20),
+      textAlign: TextAlign.center,
+    );
+  }
+}
