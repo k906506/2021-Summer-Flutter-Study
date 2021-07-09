d14f593d03a043e8eb736cf9e2f23b0e59a2e19c
k906506 <k906506@naver.com>
2021-07-09 17:17:15 +0900

    Question 추가 및 Answer 클래스로 파라미터 전달

--- myfirstflutterapp/lib/main.dart
+++ myfirstflutterapp/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -26,8 +27,14 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite food?',
+      {
+        'questionText': 'Whats your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White'],
+      },
+      {
+        'questionText': 'Whats your favorite animal?',
+        'answers': ['Dog', 'Cat', 'Lion', 'Tiger'],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -37,23 +44,12 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(
-              questions[_questionIndex],
+              questions[_questionIndex]["questionText"] as String,
             ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
-              // 괄호를 붙이면 해당 버튼이 눌렸을 때 함수가 실행되므로 리턴 값이 없음. 따라서 오류가 발생. 이를 해결하기 위해 괄호를 제거하여 포인터 값으로 전달.
-            ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: () => answerQuestion(),
-              // 이런 방법으로도 가능
-            ),
-            RaisedButton(
-                child: Text('Answer 1'),
-                onPressed: () {
-                  answerQuestion();
-                }),
+            ...(questions[_questionIndex]["answers"] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
