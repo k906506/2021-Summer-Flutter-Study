942ae7b987bbb7139d30d0069d67bbf820bf5e77
k906506 <k906506@naver.com>
2021-07-13 16:19:21 +0900

    Transaction으로 리스트 형태 출력

--- mysecondflutterapp/lib/main.dart
+++ mysecondflutterapp/lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
+
+void main() {
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries ',
+      amount: 16.99,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -9,12 +29,27 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // Column이므로 main은 y축 (위에서 아래)
           children: <Widget>[
-            Card(
-              child: Text('CHART!'),
+            Container(
+              width: double.infinity, // 기기의 최대 너비
+              child: Card(
+                color: Colors.blue,
+                elevation: 50, // 그림자
+                child: Text(
+                  'Chart',
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
-            Card(
-              child: Text('MESSAGE!'),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList(),
             ),
           ],
         ),
