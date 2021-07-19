aeaa5cd9127b11adcdfc12752a3a3013ebb41d20
k906506 <k906506@naver.com>
2021-07-19 16:28:24 +0900

    Grid 항목 클릭시 페이지 이동

--- mythirdflutterapp/lib/category_screen.dart
+++ mythirdflutterapp/lib/category_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'categories_item.dart';
+import 'dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DealMeals"),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        children: DUMMY_CATEGORIES
+            .map(
+              (cateData) => CategoryItem(
+                cateData.id,
+                cateData.title,
+                cateData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
     );
   }
 }
--- mythirdflutterapp/lib/main.dart
+++ mythirdflutterapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'category_meals_screen.dart';
+import 'category_screen.dart';
 
 void main() {
   return runApp(MyApp());
@@ -10,9 +12,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 255, 255, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
+      routes: {
+        '/categories-meals' : (ctx) => CategoryMealsScreen()
+      },
     );
   }
 }
