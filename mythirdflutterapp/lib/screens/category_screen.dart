14043dd1421f708ffe7b3424aacb648786e09515
k906506 <k906506@naver.com>
2021-07-21 16:09:43 +0900

    하단 NavigationBar 구현

--- mythirdflutterapp/lib/screens/category_screen.dart
+++ mythirdflutterapp/lib/screens/category_screen.dart
@@ -5,27 +5,22 @@ import '../dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("DealMeals"),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(20),
-        children: DUMMY_CATEGORIES
-            .map(
-              (cateData) => CategoryItem(
-                cateData.id,
-                cateData.title,
-                cateData.color,
-              ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20),
-      ),
+    return GridView(
+      padding: const EdgeInsets.all(20),
+      children: DUMMY_CATEGORIES
+          .map(
+            (cateData) => CategoryItem(
+              cateData.id,
+              cateData.title,
+              cateData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
     );
   }
 }
--- mythirdflutterapp/lib/screens/tap_screen.dart
+++ mythirdflutterapp/lib/screens/tap_screen.dart
@@ -8,6 +8,19 @@ class TapScreen extends StatefulWidget {
 }
 
 class _TapScreenState extends State<TapScreen> {
+  final List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoritesScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -15,27 +28,49 @@ class _TapScreenState extends State<TapScreen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text("Meals"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: "favorites",
-              ),
-            ],
-          ),
+          //   bottom: TabBar(
+          //     tabs: <Widget>[
+          //       Tab(
+          //         icon: Icon(
+          //           Icons.category,
+          //         ),
+          //         text: "Categories",
+          //       ),
+          //       Tab(
+          //         icon: Icon(
+          //           Icons.star,
+          //         ),
+          //         text: "favorites",
+          //       ),
+          //     ],
+          //   ),
         ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
+        body: _pages[_selectedPageIndex],
+        // TabBarView(
+        //   children: <Widget>[
+        //     CategoriesScreen(),
+        //     FavoritesScreen(),
+        //   ],
+        // ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          selectedItemColor: Theme.of(context).accentColor,
+          unselectedItemColor: Colors.white,
+          currentIndex: _selectedPageIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.category,
+              ),
+              title: Text('Categories'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.star,
+              ),
+              title: Text('Favorites'),
+            ),
           ],
         ),
       ),
