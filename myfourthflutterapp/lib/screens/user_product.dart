efec9c8f9c83aa3f9141cf5b622a4b77a3fed261
k906506 <k906506@naver.com>
2021-07-26 17:29:25 +0900

    기존에 없는 상품 추가하는 부분에서 오류발생

--- myfourthflutterapp/lib/screens/user_product.dart
+++ myfourthflutterapp/lib/screens/user_product.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
-import '/screens/edit_product_screen.dart';
-import '/widgets/app_drawer.dart';
-import '/widgets/user_product_item.dart';
-import '/providers/products.dart';
 import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+import './edit_product_screen.dart';
+
 class UserProductsScreen extends StatelessWidget {
-  static const routeName = '/user-product';
+  static const routeName = '/user-products';
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("상품 관리"),
+        title: const Text('Your Products'),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.add), onPressed: () {
-            Navigator.of(context).pushNamed(EditProductScreen.routeName);
-          }),
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+          ),
         ],
       ),
       drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-          itemBuilder: (_, index) => Column(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
             children: [
               UserProductItem(
-                  productsData.items[index].id,
-                  productsData.items[index].title,
-                  productsData.items[index].imageUrl),
+                productsData.items[i].id,
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
               Divider(),
             ],
           ),
-          itemCount: productsData.items.length,
         ),
       ),
     );
--- myfourthflutterapp/lib/widgets/user_product_item.dart
+++ myfourthflutterapp/lib/widgets/user_product_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import '/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -20,18 +23,17 @@ class UserProductItem extends StatelessWidget {
         child: Row(
           children: <Widget>[
             IconButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  EditProductScreen.routeName,
-                  arguments: id,
-                );
-              },
               icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+              },
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: () {},
               icon: Icon(Icons.delete),
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               color: Theme.of(context).errorColor,
             ),
           ],
