23251fc4f9bc251d11d6b004159c2bb4797ee960
k906506 <k906506@naver.com>
2021-07-28 09:44:46 +0900

    FireBase 연동으로 주문 목록 구현

--- myfourthflutterapp/lib/providers/products.dart
+++ myfourthflutterapp/lib/providers/products.dart
@@ -6,104 +6,104 @@ import 'product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: '농심 배흥동 비빔면, 32개입',
-      description: '엄청 시원한 비빔면 존맛탱임!',
-      price: 19020,
-      imageUrl:
-          'https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/255828096400583-70f3975f-3357-4101-bf29-d1dbc7bf94d8.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: '농심 짬뽕건면, 32개입',
-      description: '이것이 짬뽕인가 짜장인가 모르겠네',
-      price: 20300,
-      imageUrl:
-          'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/rs_quotation_api/xlezetna/983616a9d6cb4879b8ec897b0f190912.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: '신라면 블랙 두부김치, 4개입',
-      description: '히트상품 신라면 블랙의 후속작!',
-      price: 3800,
-      imageUrl:
-          'https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/11/16/18/8/c199d767-22d4-4d94-9df7-8757f5db3d34.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: '아몬드브리즈 뉴트리 식이섬유',
-      description: '다이어트 드가자',
-      price: 15480,
-      imageUrl:
-          'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/10/22/17/8/e705de8a-ec4c-4cfd-a38f-42afc47215df.jpg',
-    ),
-    Product(
-      id: 'p5',
-      title: '프레시지 더큰 햄가득 부대전골',
-      description: '집에서 해먹는 간편 부대찌개!',
-      price: 10900,
-      imageUrl:
-          'https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/06/18/12/9/9f4cd406-34aa-4e3e-ba20-2c5ebeff0234.jpg',
-    ),
-    Product(
-      id: 'p6',
-      title: '미트포유 옛날 삼겹살',
-      description: '다이어트 나가자',
-      price: 14900,
-      imageUrl:
-          'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/10/21/16/3/88432c7e-584a-4748-9db2-9760f8080ac3.jpg',
-    ),
-    Product(
-        id: 'p7',
-        title: '판토텐산 B5',
-        description: '여드름 전용인가?',
-        price: 9330,
-        imageUrl:
-            'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2019/03/20/13/1/57ecf457-bb41-4ba8-9b65-63597c024cee.jpg'),
-    Product(
-        id: 'p8',
-        title: '퀴멸의 칼날 히노카미',
-        description: '귀칼 유행이라며',
-        price: 125000,
-        imageUrl:
-            'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2021/07/13/10/4/12387f17-6f35-488c-876f-8296bd727d7c.jpg'),
-    Product(
-      id: 'p9',
-      title: '홈플래닛 스마트 체지방 측정기',
-      description: '다이어트 드가자',
-      price: 13660,
-      imageUrl:
-          'https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/519187041074607-0c17571c-5e95-469e-a940-99b2883aa25b.jpg',
-    ),
-    Product(
-        id: 'p10',
-        title: '샤오미 LCD 드로잉 테이블',
-        description: '그림이 너무 좋아',
-        price: 22950,
-        imageUrl:
-            'https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/11/04/10/5/8b59da3d-c676-4529-986d-6969690a6733.jpg'),
-    Product(
-        id: 'p11',
-        title: '닌텐도 스위치 퍼펙트 슈퍼볼',
-        description: '곧 품절예상!',
-        price: 38000,
-        imageUrl:
-            'https://thumbnail7.coupangcdn.com/thumbnails/remote/300x300ex/image/retail/images/2021/07/14/10/8/311f8e83-9b5f-4fbb-9bda-50c74560bc08.jpg'),
-    Product(
-        id: 'p12',
-        title: 'Apple 정품 USB-C 케이블',
-        description: '정품 케이블',
-        price: 19500,
-        imageUrl:
-            'https://thumbnail10.coupangcdn.com/thumbnails/remote/300x300ex/image/retail/images/330677074620956-bc96f542-d67b-45d7-91ae-f91d39b59f5b.jpg'),
-    Product(
-        id: 'p13',
-        title: '애플워치 SE, GPS',
-        description: '2021년 최신형',
-        price: 345900,
-        imageUrl:
-            'https://thumbnail7.coupangcdn.com/thumbnails/remote/300x300ex/image/retail/images/7696374737960-06c26f91-d1ad-45c8-bf34-39ee275d068a.jpg'),
+    // Product(
+    //   id: 'p1',
+    //   title: '농심 배흥동 비빔면, 32개입',
+    //   description: '엄청 시원한 비빔면 존맛탱임!',
+    //   price: 19020,
+    //   imageUrl:
+    //       'https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/255828096400583-70f3975f-3357-4101-bf29-d1dbc7bf94d8.jpg',
+    // ),
+    // Product(
+    //   id: 'p2',
+    //   title: '농심 짬뽕건면, 32개입',
+    //   description: '이것이 짬뽕인가 짜장인가 모르겠네',
+    //   price: 20300,
+    //   imageUrl:
+    //       'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/rs_quotation_api/xlezetna/983616a9d6cb4879b8ec897b0f190912.jpg',
+    // ),
+    // Product(
+    //   id: 'p3',
+    //   title: '신라면 블랙 두부김치, 4개입',
+    //   description: '히트상품 신라면 블랙의 후속작!',
+    //   price: 3800,
+    //   imageUrl:
+    //       'https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/11/16/18/8/c199d767-22d4-4d94-9df7-8757f5db3d34.jpg',
+    // ),
+    // Product(
+    //   id: 'p4',
+    //   title: '아몬드브리즈 뉴트리 식이섬유',
+    //   description: '다이어트 드가자',
+    //   price: 15480,
+    //   imageUrl:
+    //       'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/10/22/17/8/e705de8a-ec4c-4cfd-a38f-42afc47215df.jpg',
+    // ),
+    // Product(
+    //   id: 'p5',
+    //   title: '프레시지 더큰 햄가득 부대전골',
+    //   description: '집에서 해먹는 간편 부대찌개!',
+    //   price: 10900,
+    //   imageUrl:
+    //       'https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/06/18/12/9/9f4cd406-34aa-4e3e-ba20-2c5ebeff0234.jpg',
+    // ),
+    // Product(
+    //   id: 'p6',
+    //   title: '미트포유 옛날 삼겹살',
+    //   description: '다이어트 나가자',
+    //   price: 14900,
+    //   imageUrl:
+    //       'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/10/21/16/3/88432c7e-584a-4748-9db2-9760f8080ac3.jpg',
+    // ),
+    // Product(
+    //     id: 'p7',
+    //     title: '판토텐산 B5',
+    //     description: '여드름 전용인가?',
+    //     price: 9330,
+    //     imageUrl:
+    //         'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/vendor_inventory/images/2019/03/20/13/1/57ecf457-bb41-4ba8-9b65-63597c024cee.jpg'),
+    // Product(
+    //     id: 'p8',
+    //     title: '퀴멸의 칼날 히노카미',
+    //     description: '귀칼 유행이라며',
+    //     price: 125000,
+    //     imageUrl:
+    //         'https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2021/07/13/10/4/12387f17-6f35-488c-876f-8296bd727d7c.jpg'),
+    // Product(
+    //   id: 'p9',
+    //   title: '홈플래닛 스마트 체지방 측정기',
+    //   description: '다이어트 드가자',
+    //   price: 13660,
+    //   imageUrl:
+    //       'https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/519187041074607-0c17571c-5e95-469e-a940-99b2883aa25b.jpg',
+    // ),
+    // Product(
+    //     id: 'p10',
+    //     title: '샤오미 LCD 드로잉 테이블',
+    //     description: '그림이 너무 좋아',
+    //     price: 22950,
+    //     imageUrl:
+    //         'https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2020/11/04/10/5/8b59da3d-c676-4529-986d-6969690a6733.jpg'),
+    // Product(
+    //     id: 'p11',
+    //     title: '닌텐도 스위치 퍼펙트 슈퍼볼',
+    //     description: '곧 품절예상!',
+    //     price: 38000,
+    //     imageUrl:
+    //         'https://thumbnail7.coupangcdn.com/thumbnails/remote/300x300ex/image/retail/images/2021/07/14/10/8/311f8e83-9b5f-4fbb-9bda-50c74560bc08.jpg'),
+    // Product(
+    //     id: 'p12',
+    //     title: 'Apple 정품 USB-C 케이블',
+    //     description: '정품 케이블',
+    //     price: 19500,
+    //     imageUrl:
+    //         'https://thumbnail10.coupangcdn.com/thumbnails/remote/300x300ex/image/retail/images/330677074620956-bc96f542-d67b-45d7-91ae-f91d39b59f5b.jpg'),
+    // Product(
+    //     id: 'p13',
+    //     title: '애플워치 SE, GPS',
+    //     description: '2021년 최신형',
+    //     price: 345900,
+    //     imageUrl:
+    //         'https://thumbnail7.coupangcdn.com/thumbnails/remote/300x300ex/image/retail/images/7696374737960-06c26f91-d1ad-45c8-bf34-39ee275d068a.jpg'),
   ];
 
   List<Product> get items {
--- myfourthflutterapp/lib/screens/orders_screen.dart
+++ myfourthflutterapp/lib/screens/orders_screen.dart
@@ -4,42 +4,36 @@ import 'package:provider/provider.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-      super.initState();
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('주문 목록'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading ? Center(child: CircularProgressIndicator()) : ListView
-          .builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapShot) {
+          if (dataSnapShot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapShot.error != null) {
+              return Center(
+                child: Text("오류가 발생했습니다."),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
       ),
     );
   }
