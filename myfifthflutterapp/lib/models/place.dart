97bf64e5e348331abf5615ed04bd8b61661a2913
k906506 <k906506@naver.com>
2021-08-07 11:29:29 +0900

    Place Model

---
+++ myfifthflutterapp/lib/models/place.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+class Place {
+  final String id;
+  final String title;
+  final PlaceLocation location;
+  final File image;
+
+  Place(
+      {required this.id,
+      required this.title,
+      required this.image,
+      required this.location});
+}
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  PlaceLocation(this.latitude, this.longitude, this.address);
+}
